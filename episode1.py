a6275643a1a40135e7a7e72e13df163e061ad9e6
Saviolbn <saviolbn@gmail.com>
2025-08-03 11:02:37 -0300

    buttons and tasks

--- episode1.py
+++ episode1.py
@@ -16,8 +16,15 @@ background = black
 framerate = 60
 font = pygame.font.Font('freesansbold.ttf',16)
 timer = pygame.time.Clock()
+# game vars
+green_value = 1
+red_value = 2
+orange_value = 3
+white_value = 4
+purple_value = 5
 
-def draw_task(color,y_coord):
+
+def draw_task(color,y_coord,value):
     # draw on screen with a color, at these positions, with these sizes
     # a circle hollow inside, 5 line size
     pygame.draw.circle(screen, color,(30,y_coord),20,5)
@@ -25,7 +32,11 @@ def draw_task(color,y_coord):
     pygame.draw.rect(screen, color,[70, y_coord-15,200,30])
     # a black rectangle on top of the last one, -10 height and width so the line is size 5
     pygame.draw.rect(screen, black,[75, y_coord-10,190,20])
-
+    
+    #initialize text
+    value_text = font.render(str(value),True,white)
+    #render text
+    screen.blit(value_text,(16,y_coord- 10))
 
 running =True
 while running:
@@ -34,9 +45,12 @@ while running:
             running=False
             
     screen.fill(background)
-    draw_task(green,50)
-    draw_task(blue,100)
-    draw_task(red,150)
+    # task is button and progress bar
+    draw_task(green,50,green_value)
+    draw_task(red,110,red_value)
+    draw_task(orange,170,orange_value)
+    draw_task(white,230,white_value)
+    draw_task(purple,290,purple_value)
     pygame.display.flip()
     
     
